5665b2cf674b32bf5e03ae65da2d247fb142fae9
Jann Amhausend <58480669+jann-amh@users.noreply.github.com>
2021-08-10 13:43:55 +0200

    added MessageHelper

---
+++ HabibiTeaTime/Messages/MessageHelper.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TwitchLib.Client.Models;
+
+namespace HabibiTeaTime.Messages
+{
+    public static class MessageHelper
+    {
+        public static bool IsModOrBroadcaster(this ChatMessage chatMessage)
+        {
+            return chatMessage.IsModerator || chatMessage.IsBroadcaster;
+        }
+    }
+}
