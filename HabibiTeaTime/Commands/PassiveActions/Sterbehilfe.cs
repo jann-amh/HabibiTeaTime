503483ed4dc9a09a46448d3d25286a67f22ba13f
LAPTOP-4E5S1PBM\janna <j.amhausend@gmail.com>
2021-08-23 23:10:35 +0200

    added new class for the passive actions

---
+++ HabibiTeaTime/Commands/PassiveActions/Sterbehilfe.cs
@@ -0,0 +1,16 @@
+﻿using HabibiTeaTime.Twitch;
+using TwitchLib.Client.Models;
+
+namespace HabibiTeaTime.Commands.PassiveActions
+{
+    public static class Sterbehilfe
+    {
+        public static void Handle(TwitchBot bot, ChatMessage chatMessage)
+        {
+            if (chatMessage.Username == "Strbhlfe")
+            {
+                bot.Send(chatMessage.Channel, HLE.Emojis.Emoji.NerdFace);
+            }
+        }
+    }
+}
