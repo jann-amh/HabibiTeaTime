920e8eac7f428a6bf5613a338e689dfda58d2f49
LAPTOP-4E5S1PBM\janna <j.amhausend@gmail.com>
2021-11-18 10:19:49 +0100

    logger

---
+++ HabibiTeaTime/Logging/Logger.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.IO;
+using TwitchLib.Client.Models;
+
+namespace HabibiTeaTime.Logging
+{
+    public static class Logger
+    {
+        public static void Log(string text)
+        {
+            LogToFile(text);
+        }
+
+        public static void Log(ChatMessage chatMessage)
+        {
+            LogToFile($"#{chatMessage.Channel}>{chatMessage.Username}: {chatMessage.Message}");
+        }
+
+        public static void Log(Exception ex)
+        {
+            LogToFile($"{ex.GetType().Name}: {ex.Message}: {ex.StackTrace}");
+        }
+
+        private static string CreateLog(string input)
+        {
+            return $"{DateTime.Now:dd.MM.yyyy HH:mm:ss} | {input}{Environment.NewLine}";
+        }
+
+        private static void LogToFile(string log)
+        {
+            File.AppendAllText("./Resources/Logs.log", CreateLog(log));
+        }
+    }
+}
