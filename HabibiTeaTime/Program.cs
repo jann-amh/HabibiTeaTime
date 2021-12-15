b68d3fe2dc33162a2c280153b1e58f37bfea6eb7
benASTRO <81444451+benASTRO@users.noreply.github.com>
2021-12-15 20:49:03 +0100

    Update Program.cs

--- HabibiTeaTime/Program.cs
+++ HabibiTeaTime/Program.cs
@@ -13,7 +13,7 @@ namespace HabibiTeaTime
         private static void Main(string[] args)
         {
             JsonController.LoadData();
-            _ = new TwitchBot()​;
+            _ = new Twitch​Bot();
 
             while (true)
             {
